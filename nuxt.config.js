518ab9d82d52f7063bf1f8c4ee0fdb5fa36c80ef
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2021-03-11 10:01:05 -0300

    fix lint errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
